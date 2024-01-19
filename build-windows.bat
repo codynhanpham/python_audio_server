8b9dc734a1f951b8de46f8c97181ea2ba4871c3e
Cody Nhan Pham <87593612+codynhanpham@users.noreply.github.com>
2024-01-19 14:31:03 -0600

    Bundle ffmpeg stuff for Windows ; Initial MATLAB GUI

--- build-windows.bat
+++ build-windows.bat
@@ -1,3 +1,3 @@
 @REM For Windows, you must first install ffmpeg and add it to the environment variable.
 
-pyinstaller -F --name py_audio_server --add-binary="bin/async_get.exe;bin" main.py
+pyinstaller -F --name py_audio_server --add-binary="bin/async_get.exe;bin" --add-binary="bin/ffmpeg.exe;bin" --add-binary="bin/ffprobe.exe;bin" --hidden-import="scipy.special.cython_special" main.py
---
+++ start_server_windows.bat
@@ -0,0 +1,4 @@
+@echo off
+title Python Audio Server - @codynhanpham
+call .\venv\Scripts\activate
+python main.py
