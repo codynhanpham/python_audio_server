cf67697b8accceb2acf3a49d19f7ce1dfcb59282
Cody Nhan Pham <87593612+codynhanpham@users.noreply.github.com>
2024-05-14 16:08:40 -0500

    rename the app in Windows build script

--- build-windows.bat
+++ build-windows.bat
@@ -9,4 +9,4 @@ pip install -r requirements_windows.txt
 
 SET PATH=%PATH%;%CD%\bin\tbb_win
 
-pyinstaller -y --name py_audio_server --add-binary="bin/async_get.exe;bin" --add-binary="bin/ffmpeg.exe;bin" --add-binary="bin/ffprobe.exe;bin" --hidden-import="scipy.special.cython_special" --add-data="static/*;static" main.py
+pyinstaller -y --name audio_server --add-binary="bin/async_get.exe;bin" --add-binary="bin/ffmpeg.exe;bin" --add-binary="bin/ffprobe.exe;bin" --hidden-import="scipy.special.cython_special" --add-data="static/*;static" main.py
